cd4fc929ab4fe8682f8014729bd66497e596067d
Grigoriy Levanov <gregory@levanov.io>
2020-01-25 18:30:10 +0300

    add sample tasks to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { createVuexStore } from 'vuex-simple';
+import { Task } from '@/interfaces/interfaces'
 
 import { Tasks } from "@/store/tasks";
 
 Vue.use(Vuex);
 
-const instance = new Tasks([]);
+const sampleTasks:Array<Task> = [
+  {
+    date: 3,
+    text: 'Погладить кота',
+    done: true
+  },
+  {
+    date: 8,
+    text: 'Не тупить целый день на Лепре',
+    done: true
+  },
+  {
+    date: 15,
+    text: 'Купить хлеба',
+    done: false
+  },
+  {
+    date: 29,
+    text: 'Позвонить Геннадию',
+    done: false
+  }
+];
+
+const instance = new Tasks(sampleTasks);
 
 export default createVuexStore(instance, {
   strict: false,
